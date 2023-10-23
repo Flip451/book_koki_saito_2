d7d6506951f46d888811057cd8f330dcdf89c56c
Flip451 <flipflap451@gmail.com>
2023-10-23 12:25:21 +0900

    feat: add sum layer

--- projects/neural_network/src/layers/sum.rs
+++ projects/neural_network/src/layers/sum.rs
@@ -3,30 +3,29 @@ use ndarray_rand::rand_distr::num_traits::Zero;
 
 use super::layer::Layer;
 
-struct Repeat {
+struct Sum {
     n: Option<usize>,
 }
 
-struct InputOfRepeatLayer<T> {
-    input: Array1<T>,
-    n: usize,
+struct InputOfSumLayer<T> {
+    input: Array2<T>,
 }
 
-struct DInputOfRepeatLayer<T> {
-    dinput: Array1<T>,
+struct DInputOfSumLayer<T> {
+    dinput: Array2<T>,
 }
 
-struct OutputOfRepeatLayer<T> {
-    out: Array2<T>,
+struct OutputOfSumLayer<T> {
+    out: Array1<T>,
 }
 
-impl<T> Layer<T> for Repeat
+impl<T> Layer<T> for Sum
 where
     T: std::ops::Add<Output = T> + Clone + Zero,
 {
-    type Input<U> = InputOfRepeatLayer<U>;
-    type Output<U> = OutputOfRepeatLayer<U>;
-    type DInput<U> = DInputOfRepeatLayer<U>;
+    type Input<U> = InputOfSumLayer<U>;
+    type Output<U> = OutputOfSumLayer<U>;
+    type DInput<U> = DInputOfSumLayer<U>;
 
     fn new() -> Self {
         Self {
@@ -41,14 +40,14 @@ where
         let Self::Input { input, n } = input;
         self.n = Some(n);
         Self::Output {
-            out: input.broadcast((n, input.len())).unwrap().to_owned(),
+            out: input.sum_axis(Axis(0)).to_owned(),
         }
     }
 
     fn backward(&self, dout: Self::Output<T>) -> Self::DInput<T> {
         assert!(self.n.is_some());
         Self::DInput {
-            dinput: dout.out.sum_axis(Axis(0)).to_owned(),
+            dinput: dout.out.broadcast((self.n.unwrap(), dout.out.len())).unwrap().to_owned()
         }
     }
 }
