58d70e229d298943ae59c80d4e6d3df9bd13ff86
Flip451 <flipflap451@gmail.com>
2023-10-23 14:13:52 +0900

    feat: add test for repeat layer

--- projects/neural_network/src/layers/repeat.rs
+++ projects/neural_network/src/layers/repeat.rs
@@ -1,5 +1,4 @@
 use ndarray::{Array1, Array2, Axis};
-use ndarray_rand::rand_distr::num_traits::Zero;
 
 use super::layer::Layer;
 
@@ -20,20 +19,16 @@ struct OutputOfRepeatLayer {
     out: Array2<f64>,
 }
 
-impl Layer for Repeat
-{
+impl Layer for Repeat {
     type Input = InputOfRepeatLayer;
     type Output = OutputOfRepeatLayer;
     type DInput = DInputOfRepeatLayer;
 
     fn new() -> Self {
-        Self {
-            n: None,
-        }
+        Self { n: None }
     }
 
-    fn forward(&mut self, input: Self::Input) -> Self::Output
-    {
+    fn forward(&mut self, input: Self::Input) -> Self::Output {
         let Self::Input { input, n } = input;
         self.n = Some(n);
         Self::Output {
@@ -46,5 +41,43 @@ impl Layer for Repeat
         Self::DInput {
             dinput: dout.out.sum_axis(Axis(0)).to_owned(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use ndarray::array;
+
+    use super::*;
+
+    #[test]
+    fn test_repeat_layer() {
+        // test forward
+        let mut repeat = Repeat::new();
+        let input = InputOfRepeatLayer {
+            input: array![1., 2., 3., 4., 5., 6.],
+            n: 3,
+        };
+        let output = repeat.forward(input);
+
+        assert_eq!(
+            output.out,
+            array![
+                [1., 2., 3., 4., 5., 6.],
+                [1., 2., 3., 4., 5., 6.],
+                [1., 2., 3., 4., 5., 6.]
+            ]
+        );
+
+        // test backward
+        let dout = OutputOfRepeatLayer {
+            out: array![
+                [7., 8., 9., 10., 11., 12.],
+                [13., 14., 15., 16., 17., 18.],
+                [19., 20., 21., 22., 23., 24.],
+            ],
+        };
+        let dinput = repeat.backward(dout);
+        assert_eq!(dinput.dinput, array![39., 42., 45., 48., 51., 54.]);
     }
 }
