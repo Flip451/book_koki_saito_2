a99682012864cfe66210903b246288c567a13a5f
Flip451 <flipflap451@gmail.com>
2023-10-23 13:29:24 +0900

    feat: add test for branch layer

--- projects/neural_network/src/layers/branch.rs
+++ projects/neural_network/src/layers/branch.rs
@@ -41,3 +41,34 @@ impl Layer for Branch
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use ndarray::array;
+
+    use super::*;
+
+    #[test]
+    fn test_branch_layer() {
+        // test forward
+        let mut branch = Branch::new();
+        let input = InputOfBranchLayer {
+            input: array![[1., 2., 3.], [4., 5., 6.]],
+        };
+        let output = branch.forward(input);
+
+        let expected = array![[1., 2., 3.], [4., 5., 6.]];
+        assert_eq!(output.a, expected);
+        assert_eq!(output.b, expected);
+
+        // test backward
+        let dout = OutputOfBranchLayer {
+            a: array![[1., 2., 3.], [4., 5., 6.]],
+            b: array![[7., 8., 9.], [10., 11., 12.]],
+        };
+
+        let dinput = branch.backward(dout);
+        let expected = array![[8., 10., 12.], [14., 16., 18.]];
+        assert_eq!(dinput.dinput, expected);
+    }
+}
