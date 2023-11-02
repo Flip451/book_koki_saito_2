49ebc3d2d681959b529e1f62ebe58205d4616b3b
Flip451 <flipflap451@gmail.com>
2023-11-02 22:18:28 +0900

    fix: modify corpus struct

--- projects/ch02/src/Corpus.rs
+++ projects/ch02/src/Corpus.rs
@@ -17,13 +17,21 @@ impl Corpus {
 
         let mut id_to_word = HashMap::<WordId, Word>::new();
         let mut word_to_id = HashMap::<Word, WordId>::new();
+        let mut new_id = 0;
 
         let text = words
-            .enumerate()
-            .map(|(id, word)| {
-                word_to_id.insert(word.to_string(), id as WordId);
-                id_to_word.insert(id as WordId, word.to_string());
-                *word_to_id.get(word).unwrap()
+            .map(|word| {
+                let word_id = match word_to_id.get(word) {
+                    Some(id) => *id,
+                    None => {
+                        let id = new_id;
+                        new_id += 1;
+                        word_to_id.insert(word.to_string(), id);
+                        id_to_word.insert(id, word.to_string());
+                        id
+                    }
+                };
+                word_id
             })
             .collect::<Vec<WordId>>();
 
@@ -32,5 +40,40 @@ impl Corpus {
             word_to_id,
             id_to_word,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_corpus() {
+        let text = "You say goodbye and I say hello.";
+        let corpus = Corpus::new(text);
+
+        assert_eq!(corpus.text, vec![0, 1, 2, 3, 4, 1, 5, 6]);
+
+        let mut expected_word_to_id = HashMap::new();
+        expected_word_to_id.insert("you".to_string(), 0);
+        expected_word_to_id.insert("say".to_string(), 1);
+        expected_word_to_id.insert("goodbye".to_string(), 2);
+        expected_word_to_id.insert("and".to_string(), 3);
+        expected_word_to_id.insert("i".to_string(), 4);
+        expected_word_to_id.insert("hello".to_string(), 5);
+        expected_word_to_id.insert(".".to_string(), 6);
+
+        assert_eq!(corpus.word_to_id, expected_word_to_id);
+
+        let mut expected_id_to_word = HashMap::new();
+        expected_id_to_word.insert(0, "you".to_string());
+        expected_id_to_word.insert(1, "say".to_string());
+        expected_id_to_word.insert(2, "goodbye".to_string());
+        expected_id_to_word.insert(3, "and".to_string());
+        expected_id_to_word.insert(4, "i".to_string());
+        expected_id_to_word.insert(5, "hello".to_string());
+        expected_id_to_word.insert(6, ".".to_string());
+
+        assert_eq!(corpus.id_to_word, expected_id_to_word);
     }
 }
